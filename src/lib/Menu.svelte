f464a968c83aa745a4767b67174676df471f99c4
jgaboria <37048467+Noah-Whitcomb@users.noreply.github.com>
2025-03-23 16:12:48 -0400

    fixed some bugs and improved deck selector

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -1,15 +1,21 @@
 <script>
 import Deck from './Deck.svelte';
-let { decks, handleDeckChange } = $props();
+let { decks, selectedDeck, handleDeckChange } = $props();
 
 </script>
 
 <div>
 	<div class="flex flex-col gap-2">
         {#each decks as deck, index}
-          <button class="flex justify-center bg-cyan-800 text-white rounded-2xl text-2xl p-4 cursor-pointer" onclick={() => handleDeckChange(index)} >
+          {#if index === selectedDeck}
+          <div class="flex justify-center bg-cyan-800 text-white rounded-2xl text-2xl p-4 border-4 border-yellow-400 scale-105" >
             {deck.name}
-          </button>
+          </div>  
+          {:else}
+            <button class="flex justify-center hover:bg-cyan-400 bg-cyan-800 text-white rounded-2xl text-2xl p-4 cursor-pointer transition-all duration-300" onclick={() => handleDeckChange(index)} >
+              {deck.name}
+            </button>
+          {/if}
         {/each}
     </div>
 </div>
