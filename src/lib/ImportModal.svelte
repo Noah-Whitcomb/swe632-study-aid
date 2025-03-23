491a7125630d556d30bd3cf73065489c1c20fe65
CalNisson <calnisson@gmail.com>
2025-03-23 00:17:58 -0400

    Add import file formatting + template file

--- src/lib/ImportModal.svelte
+++ src/lib/ImportModal.svelte
@@ -22,6 +22,23 @@
     function closeModal() {
       dispatch("close");
     }
+
+    function downloadTemplateFile() {
+      const content = `question1 | answer1
+question2 | answer2
+question3 | answer3`;
+
+      const blob = new Blob([content], { type: "text/plain" });
+      const url = URL.createObjectURL(blob);
+
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "deck_template.txt";
+      a.click();
+
+      URL.revokeObjectURL(url);
+    }
+
   </script>
   
   <style>
@@ -52,6 +69,8 @@
       margin-top: 10px;
       padding: 8px 16px;
       cursor: pointer;
+      border: 2px solid #000;
+      border-radius: 4px;
     }
 
     input, textarea {
@@ -68,6 +87,10 @@
       <div class="modal-content" on:click|stopPropagation>
         <h2>Import Deck</h2>
         <input type="file" accept=".txt" on:change={handleFileChange} />
+        <h3>File should be .txt with each line corresponding to a flashcard</h3>
+        <h3>formatted as (without brackets):</h3>
+        <h3><strong>{'{'}question{'}'} | {'{'}answer{'}'}</strong></h3>
+        <button on:click={downloadTemplateFile}>Download Template</button>
         <button on:click={handleImport}>Import</button>
         <button on:click={closeModal}>Cancel</button>
       </div>
