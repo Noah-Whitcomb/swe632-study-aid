40d9af42fe9c05e5a3b1ccc8ec08374a3634ad18
jgaboria <37048467+Noah-Whitcomb@users.noreply.github.com>
2025-04-21 16:54:19 -0400

    removed block comments

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,4 +1,4 @@
-<!-- Header.svelte -->
+<!-- Footer.svelte -->
 
 <div class="bg-gradient-to-t from-gray-800 to-gray-600 text-white py-2 mt-auto">
     <h1 class="text-3xl text-center mb-4">
@@ -8,16 +8,3 @@
       Go Patriots!
     </p>
 </div>
-  
-
-<!-- <div class="bg-gradient-to-t from-gray-600 to-gray-800 text-white py-2 shadow-lg flex items-center">
-	<img src="{logo}" alt="Logo" class="h-16 w-auto ml-4 rounded-full border-2 border-white shadow-sm">
-	<div class="flex-1 text-center px-4">
-		<h1 class="text-4xl font-extrabold mb-2 tracking-wide text-white">
-			Welcome to the Flashcard Creator Tool (FCT)!
-		</h1>
-		<p class="text-lg mb-4 text-white">
-			Create and study flashcards to enhance your learning experience.
-		</p>
-	</div>
-</div> -->
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,20 +1,4 @@
 <!-- Header.svelte -->
-
-<!-- <script>
-	import logo from '$lib/assets/logo.png';
-</script>
-
-<div class="bg-gray-800 text-white py-2 flex items-center">
-    <img src="{logo}" alt="Logo" class="h-16 w-auto ml-4 rounded-2xl">
-    <div class="flex-1 text-center">
-        <h1 class="text-3xl font-bold mb-4 text-white">
-          Welcome to the Flashcard Creator Tool (FCT)!
-        </h1>
-        <p class="mb-8">
-          Create and study flashcards to enhance your learning experience.
-        </p>
-    </div>
-</div> -->
 <script>
 	import logo from '$lib/assets/logo.png';
 </script>
